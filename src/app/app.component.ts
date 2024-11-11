2dbfc22ecec792018d714f0dbbb5dbb89a9fcdd3
Fabricio Felix <felixfabricio115@gmail.com>
2024-11-11 19:19:58 -0300

    listar pensamentos e router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component"
 import { RodapeComponent } from "./componentes/rodape/rodape.component";
 import { CriarPensamentoComponent } from "./componentes/pensamentos/criar-pensamento/criar-pensamento.component";
 import { FormsModule } from '@angular/forms';
+import { ListarPensamentoComponent } from "./componentes/pensamentos/listar-pensamento/listar-pensamento.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CabecalhoComponent, RodapeComponent, CriarPensamentoComponent, FormsModule],
+  imports: [RouterOutlet, CabecalhoComponent, RodapeComponent, CriarPensamentoComponent, FormsModule, ListarPensamentoComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
