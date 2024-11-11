32388808e86fe49ade48571492519d976cd261ff
Fabricio Felix <felixfabricio115@gmail.com>
2024-11-11 19:38:09 -0300

    botao de router funcionando

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-listar-pensamento',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './listar-pensamento.component.html',
   styleUrl: './listar-pensamento.component.css'
 })
