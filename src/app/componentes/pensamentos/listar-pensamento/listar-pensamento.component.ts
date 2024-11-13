759dc85d7e31f352e36b3203694378b171c8bc3f
Fabricio Felix <felixfabricio115@gmail.com>
2024-11-13 19:30:51 -0300

    pensamento class

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { PensamentoComponent } from "../pensamento/pensamento.component";
 
 @Component({
   selector: 'app-listar-pensamento',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, PensamentoComponent],
   templateUrl: './listar-pensamento.component.html',
   styleUrl: './listar-pensamento.component.css'
 })
