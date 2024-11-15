9a85d9632911a212053b935ec0dca44e5faf2cfe
Fabricio Felix <felixfabricio115@gmail.com>
2024-11-15 10:42:16 -0300

    listar pensamento

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { PensamentoComponent } from "../pensamento/pensamento.component";
+import { PensamentoComponent } from '../pensamento/pensamento.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-listar-pensamento',
   standalone: true,
-  imports: [RouterModule, PensamentoComponent],
+  imports: [RouterModule, PensamentoComponent, CommonModule],
   templateUrl: './listar-pensamento.component.html',
-  styleUrl: './listar-pensamento.component.css'
+  styleUrl: './listar-pensamento.component.css',
 })
 export class ListarPensamentoComponent {
-
-  listaPensamentos = [];
-
-
-  }
+  listaPensamentos = [
+    {
+      conteudo: 'Passo Informacoes Para componente filho',
+      autoria: 'Componente filho',
+      modelo: 'modelo3',
+    },
+    {
+      conteudo: 'Minha Propriedade é decorada comn @input',
+      autoria: 'Filho',
+      modelo: 'modelo2',
+    },
+  ];
+}
--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-pensamento',
@@ -10,7 +10,7 @@ import { Component } from '@angular/core';
 export class PensamentoComponent {
 
 
-  Pensamento = {
+  @Input() pensamento = {
     conteudo: 'i love angular',
     autoria: 'ff0',
     modelo: 'modelo3',
