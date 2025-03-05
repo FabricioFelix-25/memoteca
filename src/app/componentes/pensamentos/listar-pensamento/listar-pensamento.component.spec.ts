2a9e95d0e2021cc7d81ed9cc1137f642c67a92f0
Fabricio felix <felixfabricio115@gmail.com>
2025-03-05 19:18:17 -0300

    varios erros

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.spec.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.spec.ts
@@ -21,3 +21,7 @@ describe('ListarPensamentoComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+function beforeEach(arg0: () => Promise<void>) {
+  throw new Error('Function not implemented.');
+}
+
